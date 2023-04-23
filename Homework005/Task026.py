9ed3106ef0d25279cb484cb7a8338a45de9236e8
Evgeniy <chatskiy@pm.me>
2023-04-23 20:30:05 +0300

    homework005

---
+++ Homework005/Task026.py
@@ -0,0 +1,16 @@
+﻿#Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+#*Пример:*
+#A = 3; B = 5 -> 243 (3⁵)
+#A = 2; B = 3 -> 8 
+
+a = int(input())
+b = int(input())
+res = 1                    
+def function(a, b, res):
+    if b != 0:        
+        res *= a
+        function(a, b - 1, res)
+    else:
+        print(res)
+
+function(a, b, res)
---
+++ Homework005/Task028.py
@@ -0,0 +1,21 @@
+﻿#Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. 
+#Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
+#*Пример:*
+#2 2
+#4 
+
+a = int(input())
+b = int(input())
+temp = 0
+result = 0
+def summ(a, b, result, temp):
+    if result < a:
+        result += 1
+        summ(a, b, result, temp)
+    elif temp < b:
+        result += 1
+        summ(a, b, result, temp + 1)
+    else:
+        print(result)
+
+summ(a, b, result, temp) 
