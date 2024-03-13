7b969dfbb65885c02a5bf84f8459cbc6eea565c2
EvgeniyChatskiy <120645849+EvgeniyChatskiy@users.noreply.github.com>
2024-03-13 14:18:19 +0300

    Update checkers.py

--- HW3/checkers.py
+++ HW3/checkers.py
@@ -1,17 +1,22 @@
 import subprocess
 
 
-def checkout_positive(cmd, text):
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
-    if result.returncode == 0 and text in result.stdout:
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if not result.returncode and text in result.stdout:
         return True
     else:
         return False
 
 
-def checkout_negative(cmd, text):
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
-    if result.returncode != 0 and (text in result.stderr or text in result.stdout):
+def getout(cmd):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    return result.stdout
+
+
+def negative_checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+    if result.returncode and (text in result.stdout or text in result.stderr):
         return True
     else:
         return False
